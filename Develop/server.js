c0f5ede27b2c3c0d9e61f10c2e406191f4d0ccda
Matthew Kinzle <matt.kinzle@gmail.com>
2020-07-15 15:36:50 -0500

    updated tags and server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,26 +1,69 @@
-const http = require("http");
-const fs = require('fs');
-var express = require("express");
+const fs = require("fs");
+const express = require("express");
+const path = require("path");
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-const server = http.createServer();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-server.listen(PORT, function() {
-    console.log ("Server listening on: http://localhost:" + PORT)
+const notes = []
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+  });
+
+  app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+  });
+
+  app.get("/api/notes", function (req, res) {
+    return res.json(notes)
+  });
+
+
+
+  app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    
+    notes.push(newNote)
+    console.log(notes)
+
+    res.end();
+});
+
+
+  app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:" + PORT)
 })
 
-let notes = []
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/pubic/index.html');
-})
 
-// function handleRequest(req, res) {
-//     res.end("Not today Satan!")
-// }
 
+
+// app.get('/', function(req, res) {
+//     res.send(__dirname + '/pubic/index.html');
+// })
+
+// app.get('/notes', function(req, res) {
+//     res.sendFile(__dirname + '/public/notes.html')
+// })
+
+// app.post("/api/notes", function(req, res) {
+//     var newNote = req.body;
   
+//     console.log(newNote);
   
+//     notes.push(newNote);
   
+//     res.json(newNote);
+//   });
+  
+
+
+
+
+
+
+
